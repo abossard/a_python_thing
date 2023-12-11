4f18aa53eb806f21e1b6d2a8c664dd09595d53d0
Andre Bossard <anbossar@microsoft.com>
2023-12-11 23:38:02 +0100

    Refactor order and payment models, add filename generation and blob upload

--- blobtools/blobtools/saga_blobs/step_1_generate.py
+++ blobtools/blobtools/saga_blobs/step_1_generate.py
@@ -17,13 +17,23 @@ from blobtools.config import SAGA_CONTAINER_NAME, STORAGE_ACCOUNT_CONNECTION_STR
 class Order(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     order: str
-    ordered_on: int = Field(default_factory=lambda: datetime.datetime.now())
+    ordered_on: int = Field(default_factory=lambda: int(datetime.datetime.now().timestamp()))
+
+    def get_filename(self) -> str:
+        timestamp = datetime.datetime.fromtimestamp(self.ordered_on).strftime("%Y/%m/%d")
+        return f"{timestamp}/order_{self.id}.json"
 
 class Payment(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     order_id: str
+    secret_code: str = Field(default_factory=lambda: secrets.token_hex(64))
     amount_cent: int
-    paid_on: int = Field(default_factory=lambda: datetime.datetime.now())
+    paid_on: int = Field(default_factory=lambda: int(datetime.datetime.now().timestamp()))
+
+    def get_filename(self) -> str:
+        timestamp = datetime.datetime.fromtimestamp(self.paid_on).strftime("%Y/%m/%d")
+        return f"{timestamp}/payment_{self.order_id}.json"
+    
 
 def create_random_order_payment_pair() -> Tuple[Order, Payment]:
     order = Order(order=secrets.token_hex(64))
@@ -50,13 +60,25 @@ async def main():
         container_client = blob_service_client.get_container_client(SAGA_CONTAINER_NAME)
         # create container if not exists
         
-        order_payment_pairs = [create_random_order_payment_pair() for _ in range(10)]
+        order_payment_pairs = [create_random_order_payment_pair() for _ in range(1000)]
         orders, payments = zip(*order_payment_pairs)
         order_payment_list = list(orders) + list(payments)
         random.shuffle(order_payment_list)
-        # print id and type of each item in the lit
+        
         for item in order_payment_list:
-            print(item.id, item.__class__.__name__)
+            print(item.id, item.__class__.__name__, item.get_filename())
+
+        async def upload_blob(item):
+            blob_client = container_client.get_blob_client(item.get_filename())
+            await blob_client.upload_blob(item.model_dump_json())
+            print(f"Uploaded {item.get_filename()}")
+            await blob_client.close()
+
+        upload_tasks = [upload_blob(item) for item in order_payment_list]
+        
+        await asyncio.gather(*upload_tasks)
+        await container_client.close()
+        await blob_service_client.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
