ce7ea9c10684bbaed81f426727dfadb11294c1a8
Andre Bossard <anbossar@microsoft.com>
2023-12-11 23:47:24 +0100

    Add batch processing for uploading blobs

--- blobtools/blobtools/saga_blobs/step_1_generate.py
+++ blobtools/blobtools/saga_blobs/step_1_generate.py
@@ -43,6 +43,8 @@ def create_random_order_payment_pair() -> Tuple[Order, Payment]:
 # Constants from environment variables
 
 SERVICE_NAME = "blobtools:step_1_generate"
+BATCH_SIZE = 100
+ITEMS_IN_TOTAL = 1000
 
 
 configure_opentelemetry(SERVICE_NAME)
@@ -60,23 +62,27 @@ async def main():
         container_client = blob_service_client.get_container_client(SAGA_CONTAINER_NAME)
         # create container if not exists
         
-        order_payment_pairs = [create_random_order_payment_pair() for _ in range(1000)]
+        order_payment_pairs = [create_random_order_payment_pair() for _ in range(ITEMS_IN_TOTAL)]
         orders, payments = zip(*order_payment_pairs)
         order_payment_list = list(orders) + list(payments)
         random.shuffle(order_payment_list)
         
-        for item in order_payment_list:
-            print(item.id, item.__class__.__name__, item.get_filename())
+        for i in range(0, len(order_payment_list), BATCH_SIZE):
+            trace.get_current_span().add_event(f"Uploading batch starting at {i}")
+            batch = order_payment_list[i:i+BATCH_SIZE]
+            for item in batch:
+                print(item.id, item.__class__.__name__, item.get_filename())
 
-        async def upload_blob(item):
-            blob_client = container_client.get_blob_client(item.get_filename())
-            await blob_client.upload_blob(item.model_dump_json())
-            print(f"Uploaded {item.get_filename()}")
-            await blob_client.close()
+            async def upload_blob(item):
+                blob_client = container_client.get_blob_client(item.get_filename())
+                await blob_client.upload_blob(item.model_dump_json())
+                print(f"Uploaded {item.get_filename()}")
+                await blob_client.close()
 
-        upload_tasks = [upload_blob(item) for item in order_payment_list]
+            upload_tasks = [upload_blob(item) for item in batch]
         
-        await asyncio.gather(*upload_tasks)
+            await asyncio.gather(*upload_tasks)
+        
         await container_client.close()
         await blob_service_client.close()
 
