d1fbb8939313927db7a6784aabcd7745ca84627f
Andre Bossard <anbossar@microsoft.com>
2024-01-27 17:31:03 +0100

    Refactor pizza-making process

--- pizza_maker/pizza_maker/__main__.py
+++ pizza_maker/pizza_maker/__main__.py
@@ -7,6 +7,7 @@ from opentelemetry import metrics
 from opentelemetry import trace
 
 from pizzalibrary.startup import setup_telemetry
+
 SERVICE_NAME = 'pizza_maker'
 setup_telemetry(SERVICE_NAME, "1")
 
@@ -21,15 +22,14 @@ tracer = trace.get_tracer(SERVICE_NAME)
 pizza_order_queue_name = os.getenv('PIZZA_ORDER_QUEUE_NAME')
 pizza_order_connection_string = os.getenv('PIZZA_ORDER_CONNECTION_STRING')
 
-
-with tracer.start_as_current_span("shift_start"):
-    with ServiceBusClient.from_connection_string(pizza_order_connection_string) as client:
-        with client.get_queue_receiver(queue_name=pizza_order_queue_name, max_wait_time=5) as receiver:
-            # Receive all messages
-            for msg in receiver:
-                logger.info('starting to make a pizza %s', msg)
-                pizza_counter.add(1)
-                logger.info('finished making pizza %s', msg)
-                receiver.complete_message(msg)
-    logger.info('shift end')
-    print('time to go home')
+# with tracer.start_as_current_span("shift_start"):
+with ServiceBusClient.from_connection_string(pizza_order_connection_string) as client:
+    with client.get_queue_receiver(queue_name=pizza_order_queue_name, max_wait_time=5) as receiver:
+        # Receive all messages
+        for msg in receiver:
+            logger.info('starting to make a pizza %s', msg)
+            pizza_counter.add(1)
+            logger.info('finished making pizza %s', msg)
+            receiver.complete_message(msg)
+logger.info('shift end')
+print('time to go home')
